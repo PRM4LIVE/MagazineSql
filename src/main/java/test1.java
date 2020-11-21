61dd22f4c47ae492f32122761c7bc3111d4c15ba
user <wasdwas@mail.ru>
2020-11-21 13:53:42 +0300

    Initial commit

---
+++ src/main/java/test1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class test1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = "basket"+scanner.nextLine();
+        System.out.println(str);
+    }
+}
